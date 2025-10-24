74bd98df66e0ceadf0badb13d9c99edcb2f70f05
aniket123de <aniketde1234@gmail.com>
2025-10-24 21:35:50 +0530

    fixing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
 import { HeroSection, Event, TeamSection, Footer } from "../components/index";
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="w-full min-h-screen">
